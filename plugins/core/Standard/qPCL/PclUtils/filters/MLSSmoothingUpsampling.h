2ca8e2705f90e4cbfd114a7fd590cc404d057e73
Paul RASCLE <paul.rascle@openfields.fr>
2022-05-11 18:32:29 +0200

    add a member outputCloud in PCL plugin MLSSmoothingUpsampling to get the resulting cloud in Python

--- plugins/core/Standard/qPCL/PclUtils/filters/MLSSmoothingUpsampling.h
+++ plugins/core/Standard/qPCL/PclUtils/filters/MLSSmoothingUpsampling.h
@@ -65,9 +65,11 @@ public:
 	    bool polyfit=true, int order=2, double sqrGauss=0.0001,
 	    int upsampleMethod =0, double upsampingRadius=0.01, double upsamplingStep=0.01,
 	    int stepPointDensity=4, double dilationVoxelSize=0.01, int dilationIterations=0);
+	inline ccPointCloud* getOutputCloud() { return m_outputCloud; };
 protected:
 	int getParametersFromDialog() override;
 
 protected:
 	MLSParameters m_parameters; //We directly store all the parameters here
+	ccPointCloud* m_outputCloud;
 };
