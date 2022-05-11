944af586ba306bfc9c7de0b108f6afc338841879
Paul RASCLE <paul.rascle@openfields.fr>
2022-05-11 13:56:09 +0200

    access to PCL plugin from Python: normals, smoothing

--- plugins/core/Standard/qPCL/PclUtils/filters/MLSSmoothingUpsampling.h
+++ plugins/core/Standard/qPCL/PclUtils/filters/MLSSmoothingUpsampling.h
@@ -58,9 +58,14 @@ public:
 	MLSSmoothingUpsampling();
 	~MLSSmoothingUpsampling() override;
 
-protected:
 	//inherited from BaseFilter
 	int compute() override;
+	void setParameters(ccPointCloud* cloud,
+	    bool computeNormals=true, double searchRadius=0.01,
+	    bool polyfit=true, int order=2, double sqrGauss=0.0001,
+	    int upsampleMethod =0, double upsampingRadius=0.01, double upsamplingStep=0.01,
+	    int stepPointDensity=4, double dilationVoxelSize=0.01, int dilationIterations=0);
+protected:
 	int getParametersFromDialog() override;
 
 protected:
