9cb98cf950217f94498e65cc6e84b4c7576ca725
Paul RASCLE <paul.rascle@openfields.fr>
2022-05-13 15:47:54 +0200

    update CloudCompare, fix patches for CloudComPy

--- plugins/core/Standard/qMeshBoolean/src/qMeshBoolean.cpp
+++ plugins/core/Standard/qMeshBoolean/src/qMeshBoolean.cpp
@@ -47,12 +47,12 @@
 #include <igl/copyleft/cgal/mesh_boolean.h>
 #endif
 
-//! ligIGL mesh
-struct IGLMesh
-{
-	Eigen::MatrixXd V; //!< Vertices
-	Eigen::MatrixXi F; //!< Triangles
-};
+////! ligIGL mesh
+//struct IGLMesh
+//{
+//	Eigen::MatrixXd V; //!< Vertices
+//	Eigen::MatrixXi F; //!< Triangles
+//};
 
 qMeshBoolean::qMeshBoolean(QObject* parent/*=nullptr*/)
 	: QObject(parent)
