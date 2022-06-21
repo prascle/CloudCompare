034ccf57e49ad67c0cd52b88adafe6ab1319f26c
Paul RASCLE <paul.rascle@openfields.fr>
2022-06-21 16:17:03 +0200

    add an extra parameter to LoadParameters, to give a regex filter to ply reader, extra scalar fields selection: CloudComPy issue #51)

--- libs/qCC_io/include/FileIOFilter.h
+++ libs/qCC_io/include/FileIOFilter.h
@@ -69,6 +69,7 @@ public:
 			, autoComputeNormals(false)
 			, parentWidget(nullptr)
 			, sessionStart(true)
+		    , extraData("Empty")
 		{}
 		
 		//! How to handle big coordinates
@@ -87,6 +88,8 @@ public:
 		QWidget* parentWidget;
 		//! Session start (whether the load action is the first of a session)
 		bool sessionStart;
+		//! regular expression to select extra fields (e.g. for .ply) from Python
+		QRegExp extraData;
 	};
 	
 	//! Generic saving parameters
