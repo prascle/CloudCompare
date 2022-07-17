356f048e169552a7f820de3cb338d5e976e3bf0a
Paul RASCLE <paul.rascle@openfields.fr>
2022-07-17 23:36:02 +0200

    get access from CloudComPy to guess params in plugin M3C2

--- plugins/core/Standard/qM3C2/include/qM3C2Dialog.h
+++ plugins/core/Standard/qM3C2/include/qM3C2Dialog.h
@@ -91,6 +91,10 @@ public:
 	//! Saves parameters to persistent settings
 	void saveParamsToPersistentSettings();
 
+    //! Guess parameters from the cloud #1
+    void guessParams(bool fastMode);
+    void saveParamsToGivenFile(const QString& filename);
+
 protected:
 
 	void swapClouds();
@@ -106,9 +110,6 @@ protected:
 	void updateNormalComboBox();
 
 protected: //methods
-
-	//! Guess parameters from the cloud #1
-	void guessParams(bool fastMode);
 
 	//! Sets clouds
 	void setClouds(ccPointCloud* cloud1, ccPointCloud* cloud2);
