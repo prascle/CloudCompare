b5d6acdcdfeaaf390a5904ee6c9cd79e31a7cffb
Paul RASCLE <paul.rascle@openfields.fr>
2023-01-15 11:14:31 +0100

    access to plugin qLAS from CloudComPy

--- plugins/core/IO/qLASIO/src/LasIOFilter.cpp
+++ plugins/core/IO/qLASIO/src/LasIOFilter.cpp
@@ -686,16 +686,12 @@ CC_FILE_ERROR LasIOFilter::saveToFile(ccHObject* entity, const QString& filename
 				params.extraFields.push_back(field);
 			}
 		}
+        CCTRACE("parameters.minorVersion: " << parameters.minorVersion);
+        if (parameters.minorVersion >=0)
+            params.versionMinor = parameters.minorVersion;
+        if (parameters.pointFormat >=0)
+            params.pointFormat = parameters.pointFormat;
 	}
-//	if (!parameters.alwaysDisplaySaveDialog)
-//        {
-//            CCTRACE("parameters.minorVersion: " << parameters.minorVersion);
-//            if (parameters.minorVersion >=0)
-//                params.versionMinor = parameters.minorVersion;
-//            if (parameters.pointFormat >=0)
-//                params.pointFormat = parameters.pointFormat;
-//        }
-//    CCTRACE("params.versionMinor: " << params.versionMinor);
 
 	LasSaver      saver(*pointCloud, params);
 	CC_FILE_ERROR error = saver.open(filename);
