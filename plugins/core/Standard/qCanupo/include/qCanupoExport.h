1118d60fed396ed54d47d003ed7be85f436762cb
prascle <63641539+prascle@users.noreply.github.com>
2023-02-09 13:06:44 +0100

    Windows build

--- plugins/core/Standard/qCanupo/include/qCanupoExport.h
+++ plugins/core/Standard/qCanupo/include/qCanupoExport.h
@@ -1,6 +1,6 @@
 //##########################################################################
 //#                                                                        #
-//#                              CLOUDCOMPARE                              #
+//#                       CLOUDCOMPARE PLUGIN: qM3C2                       #
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU General Public License as published by  #
@@ -11,41 +11,20 @@
 //#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
 //#  GNU General Public License for more details.                          #
 //#                                                                        #
-//#          COPYRIGHT: EDF R&D / TELECOM ParisTech (ENST-TSI)             #
+//#            COPYRIGHT: UNIVERSITE EUROPEENNE DE BRETAGNE                #
 //#                                                                        #
 //##########################################################################
 
-#ifndef PROFILE_LOADER_HEADER
-#define PROFILE_LOADER_HEADER
+#ifndef Q_CANUPO_PLUGIN_EXPORT_HEADER
+#define Q_CANUPO_PLUGIN_EXPORT_HEADER
 
-#include "qSRAExport.h"
+#include <QtCore/QtGlobal>
 
-class ccPolyline;
-class ccMainAppInterface;
+#if defined(QCANUPO_PLUGIN_LIBRARY_BUILD)
+   #define QCANUPO_PLUGIN_LIB_API Q_DECL_EXPORT
+#else
+   #define QCANUPO_PLUGIN_LIB_API Q_DECL_IMPORT
+#endif
 
-//Qt
-#include <QString>
 
-//CCCoreLib
-#include <CCGeom.h>
-
-//! Loads a 2D profile form a custom (ASCII) file
-class QSRA_PLUGIN_LIB_API ProfileLoader
-{
-public:
-
-	//! Loads a 2D profile from a file
-	/** The file must have a particular organization (see the code for more details).
-		Notably the profile is associated to a 3D origin.
-		Only the X and Y coordinates of the polyline's vertices will be used:
-		X = radius and Y = height (Z = 0).
-		\param[in] filename filename
-		\param[out] origin profile origin
-		\param[in] app main application handle for displaying messages (optional)
-		\return loaded polyline (or 0 if an error occurred)
-	**/
-	static ccPolyline* Load(QString filename, CCVector3& origin, ccMainAppInterface* app = nullptr);
-
-};
-
-#endif //PROFILE_LOADER_HEADER
+#endif //Q_CANUPO_PLUGIN_EXPORT_HEADER
