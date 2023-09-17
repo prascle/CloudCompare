d21a72cc5330b75f0b866313d0ced15cce96ee91
Paul RASCLE <paul.rascle@openfields.fr>
2023-09-17 23:43:19 +0200

    remove compilation warning deprecated endl, use Qt::endl

--- qCC/mainwindow.cpp
+++ qCC/mainwindow.cpp
@@ -9053,7 +9053,7 @@ void MainWindow::doActionComputeBestICPRmsMatrix()
 					stream << ';';
 					stream << cloud->getName();
 				}
-				stream << endl;
+				stream << Qt::endl;
 			}
 
 			//rows
@@ -9066,7 +9066,7 @@ void MainWindow::doActionComputeBestICPRmsMatrix()
 					stream << rmsMatrix[j*cloudCount+i];
 					stream << ';';
 				}
-				stream << endl;
+				stream << Qt::endl;
 			}
 
 			ccLog::Print(tr("[DoActionComputeBestICPRmsMatrix] Job done"));
@@ -9154,7 +9154,7 @@ void MainWindow::doActionExportPlaneInfo()
 	csvStream << "Nz;";
 	csvStream << "Dip;";
 	csvStream << "Dip dir;";
-	csvStream << endl;
+	csvStream << Qt::endl;
 
 	QChar separator(';');
 
@@ -9184,7 +9184,7 @@ void MainWindow::doActionExportPlaneInfo()
 		csvStream << N.z << separator;					//Nz
 		csvStream << dip_deg << separator;				//Dip
 		csvStream << dipDir_deg << separator;			//Dip direction
-		csvStream << endl;
+		csvStream << Qt::endl;
 	}
 
 	ccConsole::Print(tr("[I/O] File '%1' successfully saved (%2 plane(s))").arg(outputFilename).arg(planes.size()));
@@ -9279,7 +9279,7 @@ void MainWindow::doActionExportCloudInfo()
 			csvStream << sfIndex << " sum;";
 		}
 	}
-	csvStream << endl;
+	csvStream << Qt::endl;
 
 	//write one line per cloud
 	{
@@ -9329,7 +9329,7 @@ void MainWindow::doActionExportCloudInfo()
 				}
 				csvStream << sfSum << ';' /*"SF sum;"*/;
 			}
-			csvStream << endl;
+			csvStream << Qt::endl;
 		}
 	}
 
