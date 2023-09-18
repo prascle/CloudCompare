52264d1c9c391adaa730a71949c1bef64ddac6d3
Paul RASCLE <paul.rascle@openfields.fr>
2023-09-18 23:35:47 +0200

    (issue #130) Specify which e57 scans to import from a multi-scan e57 file

--- plugins/core/IO/qE57IO/src/E57Filter.cpp
+++ plugins/core/IO/qE57IO/src/E57Filter.cpp
@@ -1452,7 +1452,7 @@ struct LoadedScan
 	bool preserveCoordinateShift = false;
 };
 
-static LoadedScan LoadScan(const e57::Node& node, QString& guidStr, ccProgressDialog* progressDlg = nullptr)
+static LoadedScan LoadScan(const e57::Node& node, QString& guidStr, ccProgressDialog* progressDlg = nullptr, QRegExp extraData=QRegExp())
 {
 	if (node.type() != e57::E57_STRUCTURE)
 	{
@@ -1467,6 +1467,12 @@ static LoadedScan LoadScan(const e57::Node& node, QString& guidStr, ccProgressDi
 
 	//log
 	ccLog::Print(QString("[E57] Reading new scan node (%1) - %2").arg(scanNode.elementName().c_str()).arg(scanName));
+
+	if (!extraData.isEmpty() && !scanName.contains(extraData))
+	{
+	    CCTRACE("Scan node " << scanName.toStdString() << " skipped, extraData filter: " << extraData.pattern().toStdString());
+	    return {};
+	}
 
 	if (!scanNode.isDefined("points"))
 	{
@@ -2490,7 +2496,7 @@ CC_FILE_ERROR E57Filter::loadFile(const QString& filename, ccHObject& container,
 				const e57::Node scanNode = data3D.get(i);
 				QString scanGUID;
 				
-				LoadedScan scan = LoadScan(scanNode, scanGUID, showGlobalProgress ? nullptr : progressDlg.data());
+				LoadedScan scan = LoadScan(scanNode, scanGUID, showGlobalProgress ? nullptr : progressDlg.data(), parameters.extraData);
 				
 				if (scan.entity)
 				{
