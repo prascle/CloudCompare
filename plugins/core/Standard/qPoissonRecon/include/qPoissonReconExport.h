b0d8927c53814b67198c482c112190d05ec966cb
Paul RASCLE <paul.rascle@openfields.fr>
2023-09-19 19:06:28 +0200

    fix build CloudComPy on Windows

---
+++ plugins/core/Standard/qPoissonRecon/include/qPoissonReconExport.h
@@ -0,0 +1,30 @@
+//##########################################################################
+//#                                                                        #
+//#                       CLOUDCOMPARE PLUGIN: qPoissonRecon               #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 or later of the License.      #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#            COPYRIGHT: UNIVERSITE EUROPEENNE DE BRETAGNE                #
+//#                                                                        #
+//##########################################################################
+
+#ifndef Q_POISSON_RECON_PLUGIN_EXPORT_HEADER
+#define Q_POISSON_RECON_PLUGIN_EXPORT_HEADER
+
+#include <QtCore/QtGlobal>
+
+#if defined(QPOISSON_RECON_PLUGIN_LIBRARY_BUILD)
+   #define QPOISSON_RECON_PLUGIN_LIB_API Q_DECL_EXPORT
+#else
+   #define QPOISSON_RECON_PLUGIN_LIB_API Q_DECL_IMPORT
+#endif
+
+
+#endif //Q_POISSON_RECON_PLUGIN_EXPORT_HEADER
